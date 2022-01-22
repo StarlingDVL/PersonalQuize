99e9a065e8e47edc055c981bb43f645199e3b27d
ISkvor <starlingy608@gmail.com>
2022-01-22 20:54:49 +0300

    Initial Commit

---
+++ PersonalQuize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuize
+//
+//  Created by Юрий Скворцов on 22.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
